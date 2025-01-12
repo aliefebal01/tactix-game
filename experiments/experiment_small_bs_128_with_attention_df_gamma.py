14c614a01eed0640d53141d5a84aac81150c73a4
aliefebal01 <aliefebal01@gmail.com>
2025-01-12 18:27:05 +0100

    will run test with different gammas on small

--- experiments/experiment_small_bs_128_with_attention_df_gamma.py
+++ experiments/experiment_small_bs_128_with_attention_df_gamma.py
@@ -7,7 +7,7 @@ def main():
 
 
     experiment_mcts = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=100000, max_t=1000, batch_sizes=[128], layers= [[128,128,128]],
-                                                                    gammas=[0.75], epsilon_min=0.01, epsilon_max=1.0, epsilon_decay=0.9995, 
+                                                                    gammas=[0.8], epsilon_min=0.01, epsilon_max=1.0, epsilon_decay=0.9995, 
                                                                     memory_capacity=50000, device='cpu', target_update_freq=100, lr=0.0001, log_interval=1000, mcts_iteration=50, mcts_lr=0.4)
     results_mcts = experiment_mcts.run_experiments();
 
