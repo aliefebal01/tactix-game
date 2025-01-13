ad8b60a344a49d1ad5832a2926b478ba2b53c950
aliefebal01 <aliefebal01@gmail.com>
2025-01-13 03:10:54 +0100

    train pretrained models

--- experiments/experiment_small_bs_128_with_attention.py
+++ experiments/experiment_small_bs_128_with_attention.py
@@ -1,17 +1,37 @@
-
+import os
 import numpy as np
 from scripts.train_without_opp_move_attention_mcts2 import TrainAndPlot_without_opp_move_attention_mcts2
 
 def main():
 
-    pretrained_model_path_42 = "../training_results/training_results_5x5_with_attention_mcts2/models/network_hl_128_128_128_gamma_0.70_bs_128_tufq_100_mcts_iter_25_mcts_lr_0.4_wr_42_tr_-6372.pth"
+    
+    
+    
+    
+    
+    
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    pretrained_model_path_42 = os.path.join(
+        project_root,
+        "training_results",
+        "training_results_5x5_with_attention_mcts2",
+        "models",
+        "network_hl_128_128_128_gamma_0.70_bs_128_tufq_100_mcts_iter_25_mcts_lr_0.4_wr_42_tr_-6372.pth"
+    )
 
     experiment_mcts_pre_42 = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=100000, max_t=1000, batch_sizes=[128], layers= [[128,128,128]],
                                                                     gammas=[0.7], epsilon_min=0.01, epsilon_max=0.7, epsilon_decay=0.9995, 
                                                                     memory_capacity=50000, device='cpu', target_update_freq=100, lr=0.0001, log_interval=1000, mcts_iteration=50, mcts_lr=0.4)
     results_mcts = experiment_mcts_pre_42.run_experiments(pretrained_model_path=pretrained_model_path_42);
 
-    pretrained_model_path_28 = "../training_results/training_results_5x5_with_attention_mcts2/models/network_hl_128_128_128_gamma_0.70_bs_128_tufq_100_mcts_iter_50_mcts_lr_0.4_wr_28_tr_-7041.pth"
+    
+    pretrained_model_path_28 = os.path.join(
+        project_root,
+        "training_results",
+        "training_results_5x5_with_attention_mcts2",
+        "models",
+        "network_hl_128_128_128_gamma_0.70_bs_128_tufq_100_mcts_iter_50_mcts_lr_0.4_wr_28_tr_-7041.pth"
+    )
 
     experiment_mcts_pre_28 = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=100000, max_t=1000, batch_sizes=[128], layers= [[128,128,128]],
                                                                     gammas=[0.7], epsilon_min=0.01, epsilon_max=0.7, epsilon_decay=0.9995, 
--- experiments/experiment_small_bs_128_with_attention_df_gamma.py
+++ experiments/experiment_small_bs_128_with_attention_df_gamma.py
@@ -1,10 +1,18 @@
 
 import numpy as np
+import os
 from scripts.train_without_opp_move_attention_mcts2 import TrainAndPlot_without_opp_move_attention_mcts2
 
 def main():
 
-    pretrained_model_path_27 = "../training_results/training_results_5x5_with_attention_mcts2/models/network_hl_128_128_128_gamma_0.70_bs_64_tufq_100_mcts_iter_50_mcts_lr_0.4_wr_27_tr_-62915.pth"
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    pretrained_model_path_27 = os.path.join(
+        project_root,
+        "training_results",
+        "training_results_5x5_with_attention_mcts2",
+        "models",
+        "network_hl_128_128_128_gamma_0.70_bs_64_tufq_100_mcts_iter_50_mcts_lr_0.4_wr_27_tr_-62915.pth"
+    )
 
     experiment_mcts_pre_27 = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=100000, max_t=1000, batch_sizes=[128, 64], layers= [[128,128,128]],
                                                                     gammas=[0.7], epsilon_min=0.01, epsilon_max=0.7, epsilon_decay=0.9995, 
