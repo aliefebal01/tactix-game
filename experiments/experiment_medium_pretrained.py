141be9846dc0926b2ac4654970b84cef34371510
aliefebal01 <aliefebal01@gmail.com>
2025-01-22 00:27:31 +0100

    will train with end game pretrained small and medium models 500k

--- experiments/experiment_medium_pretrained.py
+++ experiments/experiment_medium_pretrained.py
@@ -12,10 +12,10 @@ def main():
         "models",
         "network_hl_256_256_256_256_256_gamma_0.70_bs_256_tufq_200_mcts_iter_50_mcts_lr_0.4_wr_24_tr_-362922.pth"
     )
-    print("training a medium model pretrained with another dqn")
-    experiment_mcts_pre_30 = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=100000, max_t=1000, batch_sizes=[128], layers= [[256, 256, 256, 256, 256]],
-                                                                    gammas=[0.7], epsilon_min=0.01, epsilon_max=0.7, epsilon_decay=0.99995, 
-                                                                    memory_capacity=50000, device='cpu', target_update_freq=100, lr=0.0001, log_interval=1000, mcts_iteration=50, mcts_lr=0.4)
+    print("training a medium model pretrained with end game")
+    experiment_mcts_pre_30 = TrainAndPlot_without_opp_move_attention_mcts2(n_episodes=500000, max_t=1000, batch_sizes=[128], layers= [[256, 256, 256, 256, 256]],
+                                                                    gammas=[0.7], epsilon_min=0.05, epsilon_max=0.7, epsilon_decay=0.99995, 
+                                                                    memory_capacity=100000, device='cpu', target_update_freq=100, lr=0.0001, log_interval=1000, mcts_iteration=50, mcts_lr=0.4)
     results_mcts = experiment_mcts_pre_30.run_experiments(pretrained_model_path=pretrained_model_path_30);
 
 if __name__ == "__main__":
